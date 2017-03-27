3440a93aa2b1e5eb86837476566b6f9843db1854
FRONT13R <FRONT13R@users.noreply.github.com>
2017-03-27 23:05:09 +0700

    Update advLib.cpp

--- advLib.cpp
+++ advLib.cpp
@@ -1,2 +1,2 @@
-//simplified ver     : https://github.com/FRONT13R/VirtualLibrary/blob/master/simplifiedLib.cpp
-//(new) instant+simple ver : https://github.com/FRONT13R/VirtualLibrary/blob/master/simple%2BinstantLib.cpp
+//simple ver               : https://github.com/FRONT13R/VirtualLibrary/blob/master/simpleLib.cpp
+//(new) simple+instant ver : https://github.com/FRONT13R/VirtualLibrary/blob/master/simple%2BinstantLib.cpp
